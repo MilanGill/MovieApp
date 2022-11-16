1e1819f33f64399bf76d70015a27ddfb146205ba
Milan_Gill <milangill@yandex.ru>
2022-11-16 21:49:02 +0300

    исправил ошибки

--- movie-category/src/main/java/com/moviecategory/moviecategory/service/controller/MovieCategoryController.java
+++ movie-category/src/main/java/com/moviecategory/moviecategory/service/controller/MovieCategoryController.java
@@ -5,7 +5,7 @@ import com.moviecategory.moviecategory.service.MovieCategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.Reque "stParam;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
